9a5b34cc4c49b810e78ec1de4a7d4e98b1704e2e
znak258 <znak258@gmail.com>
2021-11-19 17:40:09 +0900

    Spring 09
    Signed-off-by: znak258 <znak258@gmail.com>

---
+++ Spring_09/src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.springbook.biz.user.impl;
+
+import com.springbook.biz.user.UserService;
+import com.springbook.biz.user.UserVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+/* 오승필/202184-010073 */
+@Service("userService")
+public class UserServiceImpl implements UserService {
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+    @Autowired
+    private UserDAO userDAO;
+    /* 오승필/202184-010073 */
+    public void setUserDAO(UserDAO userDAO) {
+        logger.info("setUserDAO() 호출");
+        this.userDAO = userDAO;
+    }
+
+    /* 오승필/202184-010073 */
+    @Override
+    public UserVO getUser(UserVO vo) {
+        logger.info("getUser() 호출");
+        return userDAO.getUser(vo);
+    }
+}
