11fa1247329ca9ce5369a861c5cc5595aa018fa4
znak258 <znak258@gmail.com>
2021-12-02 18:31:58 +0900

    Spring 14

--- Spring_14/src/main/java/com/springbook/biz/user/impl/UserDAO.java
+++ Spring_14/src/main/java/com/springbook/biz/user/impl/UserDAO.java
@@ -2,7 +2,9 @@ package com.springbook.biz.user.impl;
 
 import com.springbook.biz.user.UserVO;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,17 +12,19 @@ import org.springframework.stereotype.Repository;
 
 /* 오승필/202184-010073 */
 @Repository
-public class UserDAO {
+public class UserDAO extends SqlSessionDaoSupport {
 	private static final Logger logger = LoggerFactory.getLogger(UserDAO.class);
-    
-	@Autowired
-	private SqlSessionTemplate sqlSessionTemplate;
+
+    @Autowired
+    public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+        super.setSqlSessionFactory(sqlSessionFactory);
+    }
+
     /* 오승필/202184-010073 */
     // CRUD 기능의 메소드 구현
     public UserVO getUser(UserVO vo){
         logger.info("===> mybatis로 getUser() 기능 처리");
-    	return (UserVO) sqlSessionTemplate.selectOne("UserDAO.getUser", vo);
-//    	return (UserVO) vo;
+    	return (UserVO) getSqlSession().selectOne("UserDAO.getUser", vo);
 
     }
 }
